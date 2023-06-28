a75ac9d3ee6a12e3276c1e330a02c3dfa5f5b6d1
rocalex <rocalex82@gmail.com>
2023-06-29 00:08:41 +0800

    fix main nav

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { useState } from "react";
 
 export default function MainNav() {
+  const [isActive, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!isActive);
+  };
+
   return (
     <nav className="navbar is-primary">
       <div className="container">
@@ -8,10 +15,21 @@ export default function MainNav() {
           <Link className="navbar-item" href="/">
             <strong>FirstCall</strong>
           </Link>
+          <a
+            role="button"
+            className={`navbar-burger ${isActive ? "is-active" : ""}`}
+            onClick={handleClick}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
         </div>
-        <div className="navbar-menu">
+        <div className={`navbar-menu ${isActive ? "is-active" : ""}`}>
           <div className="navbar-start">
-            <Link className="navbar-item" href={"/users"}>Users</Link>
+            <Link className="navbar-item" href={"/users"}>
+              Users
+            </Link>
           </div>
         </div>
       </div>
